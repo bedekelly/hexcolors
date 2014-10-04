83a92ebf5655329cd29faafa0813cb2706cdf10f
Bede Kelly <bedekelly@users.noreply.github.com>
2014-10-04 21:46:21 +0100

    Update hexcolors.py

--- hexcolors.py
+++ hexcolors.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import subprocess
-import sys
+import sys  # so heavy, but apparently just putting "unicode" makes people cry
 
-# 2B1B is the larger unicode square; 25A0 is smaller.
+# u+2588 is the full unicode block; 25A0 is a smaller square.
 uni_square = bytes('\\u2588', 'ascii').decode('unicode-escape')
 
 # Redefine a terminal color to use in this script.
@@ -19,9 +20,9 @@ while True:
     try:
         hexcode = input("Hex code (e.g. FFFFFF): ")
     except (KeyboardInterrupt, EOFError):
-        print(); quit()
+        print(); quit()  # whatever
     subprocess.call("clear")
     color_str = "{}/{}/{}".format(hexcode[0:2], hexcode[2:4], hexcode[4:])
-    printf_string = begin_str + color_str + end_str
+    printf_string = begin_str + color_str + end_str  # String concatenation, and what
     # Using printf, since the print() function doesn't like ANSI escapes.
     subprocess.call(["printf", "{}\n".format(printf_string)])
