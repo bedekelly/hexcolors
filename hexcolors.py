26a35774899fec19854fce5d8a65e0b9480bc958
Bede Kelly <bedekelly97@gmail.com>
2015-03-17 17:08:18 +0000

    Fill up the terminal window.

--- hexcolors.py
+++ hexcolors.py
@@ -9,21 +9,23 @@ uni_square = bytes('\\u2588', 'ascii').decode('unicode-escape')
 # Redefine a terminal color to use in this script.
 begin_str = '\033]4;18;rgb:'
 
-# Get the terminal width
+# Go out to shell for an accurate terminal width.
 enc = sys.stdout.encoding
 cols = int(subprocess.check_output(["tput", "cols"]).decode(enc))
+lines = int(subprocess.check_output(["tput", "lines"]).decode(enc))
+lines -= 1  # Make room for the input box.
 
-# Print out the square a good few times. Also fix the color back to normal.
+# Print out the square, and reset the colour afterwards.
 end_str = "\a\033[38;5;18m{}\033[m".format(uni_square*cols)
-
 
 while True:
     try:
         hexcode = input("Hex code (e.g. FFFFFF): ")
     except (KeyboardInterrupt, EOFError):
-        print(); quit()  # whatever
+        break
     subprocess.call("clear")
     color_str = "{}/{}/{}".format(hexcode[0:2], hexcode[2:4], hexcode[4:])
-    printf_string = begin_str + color_str + end_str  # String concatenation, and what
-    # Using printf, since the print() function doesn't like ANSI escapes.
-    subprocess.call(["printf", "{}\n".format(printf_string)])
+    printf_string = "{}{}{}".format(begin_str, color_str, end_str)
+    for _ in range(lines):
+        # Using system printf for correct escape-sequence support.
+        subprocess.call(["printf", "{}\n".format(printf_string)])
